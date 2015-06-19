52f48a22c5af48a77f84e28c8c4c45ae3fa72957
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2015-06-19 16:24:13 +0800

    update get_sdk.sh

--- get_sdk.sh
+++ get_sdk.sh
@@ -1,22 +1,25 @@
 # exit the script if any statement fails
 set -e
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 SHORT_SDK_NAME=sdk
 FULL_SDK_NAME=Neurotec_Biometric_5_1_SDK_Trial
 
-if [ ! -d $SHORT_SDK_NAME ];
+if [ ! -d $DIR/$SHORT_SDK_NAME ];
 then
-  if [ ! -f $SHORT_SDK_NAME.zip ];
+  if [ ! -f $DIR/$SHORT_SDK_NAME.zip ];
   then
-    wget -O $SHORT_SDK_NAME.zip http://download.neurotechnology.com/$FULL_SDK_NAME\_2015-03-02.zip
+    wget -O $DIR/$SHORT_SDK_NAME.zip http://download.neurotechnology.com/$FULL_SDK_NAME\_2015-03-02.zip
   fi
-  unzip $SHORT_SDK_NAME.zip \
+  unzip $DIR/$SHORT_SDK_NAME.zip \
     "$FULL_SDK_NAME/Bin/Data/*" \
     "$FULL_SDK_NAME/Bin/Linux_x86_64/Activation/*" \
     "$FULL_SDK_NAME/Lib/Linux_x86_64/*" \
     "$FULL_SDK_NAME/Tutorials/Biometrics/C/*" \
     "$FULL_SDK_NAME/Tutorials/Common/Build/*" \
     "$FULL_SDK_NAME/Tutorials/Common/C/*" \
-    "$FULL_SDK_NAME/Include/*"
-  mv $FULL_SDK_NAME $SHORT_SDK_NAME
+    "$FULL_SDK_NAME/Include/*" \
+    -d $DIR
+  mv $DIR/$FULL_SDK_NAME $DIR/$SHORT_SDK_NAME
 fi
